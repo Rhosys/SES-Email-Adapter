92a786eb1d1278f4af5ed03cfbc6050227b15620
Warren Parad <gpg@warrenparad.net>
2026-05-14 13:00:06 +0200

    🟣 Route /assets/* through versioned S3 origin for correct key resolution

--- deploy/cdn.tf
+++ deploy/cdn.tf
@@ -110,7 +110,7 @@ resource "aws_cloudfront_distribution" "api" {
     path_pattern           = "/assets/*"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = local.s3_site_origin_id
+    target_origin_id       = local.s3_assets_origin_id
     cache_policy_id        = aws_cloudfront_cache_policy.assets_cache.id
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
